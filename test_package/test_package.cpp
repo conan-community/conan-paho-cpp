bde2e352d3db79629241abf1313d4ec12d66a49c
danimtb <danimanzaneque@gmail.com>
2018-07-18 12:38:14 +0200

    updated test_package [skip ci]

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -5,16 +5,15 @@
 const std::string SERVER_ADDRESS { "tcp://localhost:1883" };
 const std::string CLIENT_ID { "consume" };
 
-/////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char* argv[])
 {
-	mqtt::connect_options connOpts;
-	connOpts.set_keep_alive_interval(20);
-	connOpts.set_clean_session(true);
+    mqtt::connect_options connOpts;
+    connOpts.set_keep_alive_interval(20);
+    connOpts.set_clean_session(true);
 
-	mqtt::async_client cli_async(SERVER_ADDRESS, CLIENT_ID);
+    mqtt::async_client cli_async(SERVER_ADDRESS, CLIENT_ID);
     mqtt::client cli(SERVER_ADDRESS, CLIENT_ID);
 
-	return 0;
+    return 0;
 }
